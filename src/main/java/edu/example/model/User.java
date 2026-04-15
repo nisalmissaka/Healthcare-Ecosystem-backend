dea44a229276002091499ac526d322683c24d70c
nisalmissaka <wishwakanisal@gmail.com>
2026-04-15 14:20:17 +0530

    create to User.java class

---
+++ src/main/java/edu/example/model/User.java
@@ -0,0 +1,15 @@
+package edu.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+    private String username;
+    private String password;
+}
