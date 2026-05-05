7a3f8bb3f4245844333b1e7752114386a21afd2e
nisalmissaka <wishwakanisal@gmail.com>
2026-05-05 17:27:54 +0530

    create User.java

--- src/main/java/edu/example/model/User.java
+++ src/main/java/edu/example/model/User.java
@@ -1,15 +1,26 @@
 package edu.example.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Entity
+@Table(name = "app_users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String username;
-    private String password;
+
+    @Column(nullable = false)
+    private String email;
 }
